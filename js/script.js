a99a67c4b02883c8f6c8516f576c91f083ef3bff
toxicmaster <182410102035@cs.unej.ac.id>
2019-11-20 08:19:08 +0700

    update jquery

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-const menu = document.getElementById('menu');
-menu.addEventListener('click',function(){
-    let sidebar = document.querySelector('.sidebar');
-    sidebar.style.position = "fixed";
-    sidebar.style.right = "0px";
-    sidebar.style.top = "0px";
-})
+$(document).ready(function(){
+    $(".sidebar").hide();
+    $("#menu").click(function(){
+        $(".sidebar").show(500);
+    });
+    
+    $(".close").click(function(){
+        $(".sidebar").hide(500);
+    }); 
 
-const close = document.querySelector('.close');
-close.addEventListener('click',function(){
-    let sidebar = document.querySelector('.sidebar');
-    sidebar.style.position = "fixed";
-    sidebar.style.right = "-200px";
-    sidebar.style.top = "0px";
-})
+    $(".card").click(function(){
+        if($(this).index() == 1){
+            $(".dest-inner h1").text("Pantai Papuma");
+        } else if($(this).index() == 2){
+            $(".dest-inner h1").text("Kawah Ijen");
+        } else if($(this).index() == 3){
+            $(".dest-inner h1").text("J88");
+        } else if($(this).index() == 4){
+            $(".dest-inner h1").text("Pantai Payangan");
+        } else if($(this).index() == 0){
+            $(".dest-inner h1").text("Gunung Bromo");
+        } 
+
+    });
+    
+
+});
+
